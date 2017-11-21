50256e632cad83288ec62faa3b944ff5a8e64e64
Sam Gillingham <gillingham.sam@gmail.com>
2017-11-21 11:33:02 +1000

    undo last commit as i pushed into the wrong repo - sorry

--- recipe/build.sh
+++ recipe/build.sh
@@ -72,7 +72,6 @@ export CPPFLAGS="$CPPFLAGS -I$PREFIX/include"
             --with-libz=$PREFIX \
             --with-libkml=$PREFIX \
             --with-libtiff=$PREFIX \
-            --with-geotiff=$PREFIX \
             --with-liblzma=yes \
             --with-netcdf=$PREFIX \
             --with-openjpeg=$PREFIX \
